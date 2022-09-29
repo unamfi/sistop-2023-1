2363c835caa5926093bf33163d5560598d22e499
ErickJa <kikis00574@gmail.com>
2022-09-29 02:27:01 -0500

    Acaba el programa con mi nombre y un mensaje

---
+++ practicas/3/MendezErick/practica3.java
@@ -0,0 +1,6 @@
+public class practica3{
+    public static void main(String []args){
+        System.out.println("\n Méndez Sánchez Erick Jair");
+        System.out.println("Esta es mi prueba de la práctica 3");
+    }
+}
