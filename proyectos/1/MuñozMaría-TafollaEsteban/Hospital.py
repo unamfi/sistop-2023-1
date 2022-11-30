dc8d7bdd44c9fe890c64e7ce95d4894cf12fb989
=taes <taestafolla@gmail.com>
2022-11-30 03:38:11 -0600

    Agregando interfaz 3

--- proyectos/1/MuñozMaría-TafollaEsteban/Hospital.py
+++ proyectos/1/MuñozMaría-TafollaEsteban/Hospital.py
@@ -1,3 +1,4 @@
+#Version con consultorios aleatorios
 import threading
 import time
 import random
@@ -8,53 +9,39 @@ from tkinter import Canvas
 #Total de consultorios
 
 n_consultorios = 4
-multiplex_consultorios = threading.Semaphore(n_consultorios) #consultorios ocupados
-multiplex_emergencia = threading.Semaphore(n_consultorios) # Verifica que alguien con emergencia pase antes que cualquiera
-multiplex_regulares= threading.Semaphore(1) #Verifica que alguien con cita pase antes que alguien sin cita
-mutex_copia =  threading.Semaphore(1)
-GUI_mutex_fila = threading.Semaphore(5)
-mutex_protege_incrementos = threading.Semaphore(1)
-#consultorio_disponible = [0,0,0,0]
-root = tkinter.Tk()
-canvas = Canvas(root,width = 1050, height = 600)
-tamanio = (150,150)
-GUI_cont_fila = 0
-GUI_cont_cons = 0
-GUI_fila = []
-GUI_consultorios = []
-'''
-def buscaConsultorio():
-    global consultorio_disponible
-    cont = 0
-    while(consultorio_disponible[cont]):
-        cont+=1
-    consultorio_disponible[cont] = 1
-    print (cont)
-    return cont+1
-def liberaConsultorio(num_consultorio):
-    global consultorio_disponible
-    consultorio_disponible[num_consultorio] = 0
-'''
+multiplex_consultorios = threading.Semaphore(n_consultorios) #Consultorios disponibles en el hospital
+#multiplex_emergencia = threading.Semaphore(n_consultorios) # Verifica que alguien con emergencia pase antes que cualquiera
+mutex_copia =  threading.Semaphore(1) #Protege acceso a informacion para la interfaz
+GUI_mutex_fila = threading.Semaphore(8) #Representa las imagenes que puede haber dentro de la interfaz
+mutex_protege_incrementos = threading.Semaphore(1) #Protege a la hora de obtener el tamaño de nuestra fila
+root = tkinter.Tk() #Crea nuestra ventana para la interfaz
+canvas = Canvas(root,width = 1050, height = 600) #Crea un espacio de travajo dentro de la ventana
+tamanio = (150,150) #Vector que define las dimenciones de las imagenes de nuestros pacientes
+GUI_fila = [] #Representa a las persona formadas dentro de la fila  
+
+#Funciones para realizar el movimiento de las personas
 def up(canvaImagen):
     canvas.move(canvaImagen,0,-5)
     root.update()
-    #time.sleep(.01)
+    
 
 def down(canvaImagen):
     canvas.move(canvaImagen,0,5)
     root.update()
-    #time.sleep(.01)
+    
 
 def left(canvaImagen):
     canvas.move(canvaImagen,-5,0)
     root.update()
-    #time.sleep(.01)
+    
 
+#Clase para generar citas para el paciente regular
 class Cita: 
     def __init__(self,hora ,minuto):
         self.hora = hora
         self.minuto = minuto
-        
+
+#Clase para medir tiempo 
 class Reloj:
     def __init__(self):
         self.hora = 7
@@ -70,14 +57,11 @@ class Reloj:
             self.minutos = 0
         else:
             self.minutos += 15
-        
+#Realiza la impresion del reloj dentro de la interfaz
 def relojito():
     global root
     global reloj1
     print("7:00")
-    #GUI_reloj = StringVar()
-    #GUI_reloj.set("7:00")
-    #tkinter.Label.config(textvariable=GUI_reloj)
     GUI_reloj = tkinter.Label(root,text="7:00")
     GUI_reloj.config(font=("Verdana",24)) 
     GUI_reloj.pack(anchor="nw")
@@ -92,7 +76,8 @@ def relojito():
             print(str(reloj1.hora)+":"+str(reloj1.minutos))
             GUI_reloj.config(text=str(reloj1.hora)+":"+str(reloj1.minutos))
         root.update()
-        
+
+#Hilos que generan mas hilos de forma ciclica
 def HiloEmergencia():
     id=1
     time.sleep(1)
@@ -105,7 +90,6 @@ def HiloRegular():
     id=1
     time.sleep(random.randint(1,2))
     while True:
-        #icono = generaImagen("Regular", tamanio)
         threading.Thread(target=regular,args=[id]).start()
         time.sleep(random.randint(1,2))
         id+=1
@@ -117,22 +101,24 @@ def HiloChequeo():
         threading.Thread(target=chequeo,args=[id]).start()
         time.sleep(random.randint(1,2))
         id+=1
+
+#Recorre los elementos ubicados dentro de la fila
 def avanzaFila(GUI_fila):
     for i in range (0, len(GUI_fila)):
         for j in range (1,30):
                 left(GUI_fila[i])
-    
+
+#Permite que avance hacia algun consultorio uno de los pacientes ubicados en la fila    
 def avanzaConsultorio(GUI_fila, noConsultorio):
     if(noConsultorio==1):
         for i in range (0, 60):
             left(GUI_fila[0])
-        return GUI_fila.pop()
+        return GUI_fila.pop(0)
     elif(noConsultorio==2):
         for i in range (0, 60):
+            left(GUI_fila[0])
             if(i < 40):
                 up(GUI_fila[0])
-            left(GUI_fila[0])
-            
         return GUI_fila.pop(0)
     elif(noConsultorio==3):
         for i in range (0, 60):
@@ -148,143 +134,113 @@ def avanzaConsultorio(GUI_fila, noConsultorio):
             up(GUI_fila[0])
         return GUI_fila.pop(0)
 
+#Hilo de paciente regular
 def regular(id):
-    global reloj1
-    global GUI_cont_fila
+    global reloj1 
     global GUI_fila
     global tamanio
     #Se crea la cita. Cada numero de cita representa una hora en específico
     #1 son las 7:00, 2 son las 7:15, 60 son las 22:00, ...
-
     cita1 = Cita(random.randint(reloj1.hora-2,reloj1.hora+2), random.randint(0,3)*15)
 
     #Un paciente regular puede llegar (mas o menos) una hora antes de su cita y ser atendido.
     #si llega antes o después no se le atendera
     if cita1.hora-reloj1.hora <= 1 and  cita1.hora-reloj1.hora >= 0:
-        multiplex_emergencia.acquire()
-        multiplex_emergencia.release()
+        #Entra a la fila
         GUI_mutex_fila.acquire()
+        #Crea la imagen dentro de la interfaz grafica
         GUI_icono = generaImagen("Regular", tamanio)
+        #Usamos un mutex para evitar alteraciones dentro de los datos que obtendremos
         mutex_protege_incrementos.acquire()
         GUI_fila.append(canvas.create_image(1200,500, image = GUI_icono))    
-        posision=150-30*(len(GUI_fila)-1)
-        print("La posicion de regular es", str(posision))
-        for i in range (1,posision):
+        posicion=150-30*(len(GUI_fila)-1)
+        #Coloca la imagen dentro de la interfaz
+        for i in range (1,posicion):
             left(GUI_fila[len(GUI_fila)-1])
         mutex_protege_incrementos.release()
-
-        
-
-        multiplex_regulares.acquire()
+        #Ingresa al consultorio
         multiplex_consultorios.acquire()
         print("Soy un paciente regular",id,"y acabo de entrar a mi cita. La cita era a las "+str(cita1.hora)+":"+str(cita1.minuto))
-        
-        time.sleep(5)
-        print("Soy el paciente regular",id,"y ya salí de consulta")
-        
-        #Elimina un elemento grafico
+        #Protege la funcion avanzaConsultorio
         mutex_copia.acquire()
-        #consultrio_asignado =  buscaConsultorio()
-        #avanzaConsultorio(GUI_fila,random.randint(1,4))
-        GUI_fila.pop(0)
+        avanzaConsultorio(GUI_fila, random.randint(1,4))
         avanzaFila(GUI_fila)
         mutex_copia.release()
-        #liberaConsultorio(consultrio_asignado-1)
+        time.sleep(5)
+        print("Soy el paciente regular",id,"y ya salí de consulta")
         GUI_mutex_fila.release()
-        multiplex_regulares.release()
         multiplex_consultorios.release() #fin de consulta
-        
     
     else:
+        #En caso de no llegar a cita
         print("Soy un paciente regular y como no llegué a la hora de mi cita me mandaron pa mi casa :( \nMi cita era a las "+str(cita1.hora)+":"+str(cita1.minuto))
     
-
+#Hilo de paciente emergencia
 def emergencia(id):
     global GUI_fila
     global tamanio
+
+    #Hace fila
     GUI_mutex_fila.acquire()
     
-    GUI_icono = generaImagen("Emergencia", tamanio)
-   
-     
+    #Genera imagen de interfaz
+    GUI_icono = generaImagen("Emergencia", tamanio) 
+    #Protege acceso a la informacion
     mutex_protege_incrementos.acquire()
     GUI_fila.append(canvas.create_image(1200,500, image = GUI_icono))   
     posision=150-30*(len(GUI_fila)-1)
-    print("La posicion de emergencia es", str(posision))
     for i in range (1,posision):
         left(GUI_fila[len(GUI_fila)-1])
     mutex_protege_incrementos.release()
 
     
-    
-    multiplex_emergencia.acquire()
     multiplex_consultorios.acquire()
     print("Soy el paciente con emergencia "+str(id)+".He pasado directo al consultorio")
-    
+    #protegeFuncion avanzaConsultorio
+    mutex_copia.acquire()
+    avanzaConsultorio(GUI_fila, random.randint(1,4))
+    avanzaFila(GUI_fila)
+    mutex_copia.release()
     time.sleep(10)
     print("Soy el paciente con emergencia "+str(id)+" y ya me voy")
     
-    #Elimina un elemento grafico
-    mutex_copia.acquire()
-        #consultrio_asignado =  buscaConsultorio()
-        #avanzaConsultorio(GUI_fila,random.randint(1,4))
-    GUI_fila.pop(0)
-    avanzaFila(GUI_fila)
-    mutex_copia.release()
-    #liberaConsultorio(consultrio_asignado-1)
     GUI_mutex_fila.release()
-    multiplex_emergencia.release()
     multiplex_consultorios.release() #fin de consulta
     
     
-    
 def chequeo(id):
-    global GUI_consultorios
     global GUI_fila
     global tamanio
     #Verifica que cumple las condiciones para hacer fila
-    multiplex_emergencia.acquire()
-    multiplex_emergencia.release()
-
-    multiplex_regulares.acquire()
-    multiplex_regulares.release()
     GUI_mutex_fila.acquire()
 
     #Crea su representacion grafica
     GUI_icono = generaImagen("Chequeo", tamanio)
-      
+    #Protege datos de GUI_fila  
     mutex_protege_incrementos.acquire()
     GUI_fila.append(canvas.create_image(1200,500, image = GUI_icono))  
     posision=150-30*(len(GUI_fila)-1)
-    print("La posicion de cheaquo es", str(posision))
     for i in range (1,posision):
         left(GUI_fila[len(GUI_fila)-1])
     mutex_protege_incrementos.release()
 
-    
-    
-    #verifica que haya consultorios disponibles y resive consulta    
+    #Verifica que haya consultorios disponibles y resive consulta    
     multiplex_consultorios.acquire()
     print("Soy el paciente de chequeo "+str(id)+". He pasado al consultorio")
-    
-    time.sleep(5)    
-    
-    print("El paciente de chequeo "+str(id)+" se a ido") 
-    #Elimina un elemento grafico
     mutex_copia.acquire()
-        #consultrio_asignado =  buscaConsultorio()
-        #avanzaConsultorio(GUI_fila,random.randint(1,4))
-    GUI_fila.pop(0)
+    avanzaConsultorio(GUI_fila, random.randint(1,4))
     avanzaFila(GUI_fila)
     mutex_copia.release()
-    #liberaConsultorio(consultrio_asignado-1)
+    time.sleep(5)
+    print("El paciente de chequeo "+str(id)+" se a ido")
     GUI_mutex_fila.release()
     multiplex_consultorios.release() #fin de consulta
-    
 
-"""
-QUITAR DE AQUI BUSCAR IMAGEN Y GENERAIMAGEN PARA EVITAR COCHINERO
-"""
+#Obtiene imagen de directorio cada uno de los doctores representa un tipo de paciente 
+# Verde = Regular
+# Rojo = Emergencia
+# Azul = Chequeo
+
 def buscaImagen(tipo):
     aleatorio = random.randint(0, 5)   
     if(tipo=="Emergencia"):
@@ -294,6 +250,7 @@ def buscaImagen(tipo):
     elif(tipo=="Regular"):
         return "imagenes/imageV"+str(aleatorio)+".jpg"
 
+#Crea imagen con el tamaño de la variable tamanio
 def generaImagen(tipo, tamanio):
     imname = buscaImagen(tipo)
     return  ImageTk.PhotoImage(Image.open(imname).resize(tamanio))
@@ -305,54 +262,21 @@ def iniciaPrograma():
     global canvas
     canvas.pack()
 
+    #Fondo de la interfaz
     fondo = ImageTk.PhotoImage(file = "imagenes/Background.jpeg")
-    #res_fondo = tkinter.Label(root, image = fondo, bd = 0)
-    #res_fondo.place(relx=0, rely=0)
-
     canvas.create_image(0,0,image = fondo, anchor="nw")
 
-    """im2 = generaImagen("Chequeo", tamanio)
-    canvaImagen = canvas.create_image(75,75, image = im2)
-
-    label = tkinter.Label(root, image= generaImagen("Regular", (100,100)))
-    label.place(x=0,y=0)
-
-    im2 = generaImagen("Chequeo", tamanio)
-    canvaImagen = canvas.create_image(75,75, image = im2)
-    
-    for i in range (1,100):
-        canvas.move(canvaImagen,2,2)
-        root.update()
-        time.sleep(.01)
-
-    """
+    #Inicia hilos
     threading.Thread(target=relojito).start()
     threading.Thread(target=HiloRegular).start()
     threading.Thread(target=HiloChequeo).start()
     threading.Thread(target=HiloEmergencia).start()
+    
+    #Inicia interfaz
     root.mainloop()
     
-
+#Inicia reloj
 reloj1 = Reloj()
+
 iniciaPrograma()
 
-#---------------------------------------------
-
-def buscaImagen(tipo):
-    aleatorio = random.randint(0, 5)   
-    if(tipo=="Emergencia"):
-        return "imagenes/imageR"+str(aleatorio)+".jpg"
-    elif(tipo=="Chequeo"):
-        return "imagenes/imageA"+str(aleatorio)+".jpg"
-    elif(tipo=="Regular"):
-        return "imagenes/imageV"+str(aleatorio)+".jpg"
-
-def generaImagen(tipo, tamanio):
-    imname = buscaImagen(tipo)
-    return  ImageTk.PhotoImage(Image.open(imname).resize(tamanio))
-
-
-"""
-Chat 
-
-"""
